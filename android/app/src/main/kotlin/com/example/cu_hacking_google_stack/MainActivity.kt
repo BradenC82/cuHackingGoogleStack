4656c75d07a1a67f14be43504cbe8ee2bf478a0d
Braden Collingwood <bradencollingwood82@gmail.com>
2020-01-11 13:25:19 -0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/cu_hacking_google_stack/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cu_hacking_google_stack
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
